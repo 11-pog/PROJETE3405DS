d27868fc6e5c6e7b26c599838f593797707071bb
OpinionThief <82518058+11-pog@users.noreply.github.com>
2025-06-05 09:32:29 -0300

    Mudada um pouquinho a função sla xd

--- coisas/teste.py
+++ coisas/teste.py
@@ -1,4 +1,4 @@
-from typing import Any, Iterable, List, Set
+from typing import Any, List, Optional, Set
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -6,25 +6,30 @@ import matplotlib.pyplot as plt
 # oi
 
 
-def find_related(G: nx.Graph, starting_point: Any, depth: int | slice = 1) -> Set[Any] | List[Set[Any]]:
-    _max = depth + 1 if isinstance(depth, int) else depth.stop
-    _found: List[Set[Any]] = [set() for _ in range(_max)]
+def find_related(G: nx.Graph, starting_point: Any, depth: int | slice = 1, set_per_layer: Optional[bool] = True) -> Set[Any] | List[Set[Any]]:
+    _max = depth if isinstance(depth, int) else depth.stop
+    _found: List[Set[Any]] = [set() for _ in range(_max + 1)]
     
     def _find(n: int) -> None:
         for point in _found[n - 1]:
+            _last_found = _found[max(n - 2, 0)]
             for neighbor in G.neighbors(point):
-                if all(neighbor not in s for s in _found[:n + 1]) and neighbor != starting_point:
+                if neighbor not in _last_found:
                     _found[n].add(neighbor)
         
-        if n < _max - 1:
+        if n < _max:
             _find(n + 1)
     
-    _found[0] = set(G.neighbors(starting_point))
+    _found[0] = set([starting_point])
     _find(1)
-    return _found[depth]
+    
+    _final_depth = depth + 1 if isinstance(depth, int) else slice(depth.start + 1, depth.stop + 1, depth.step)
+    _result = _found[_final_depth] if set_per_layer or isinstance(depth, int) else [item for items in _found[_final_depth] for item in items]
+    
+    return _result
 
 
-G: nx.Graph = nx.erdos_renyi_graph(n=15, p=0.35)
+G: nx.Graph = nx.grid_2d_graph(7, 7)
 
 '''
 G.add_edge("protagas", "roberto")
@@ -36,11 +41,10 @@ G.add_edge("fabiano", "nichalas")
 G.add_edge("fabiano", "protagas")
 '''
 
-found = find_related(G, 7, slice(1, 4))
+found = find_related(G, (3,3), slice(0, 4), set_per_layer=True)
 
 print("ACHADOS PELA FUNÇÃO")
 print(found)
-
 
 nx.draw(G, with_labels=True, node_color='skyblue', node_size=800, font_weight='bold')
 plt.show()
