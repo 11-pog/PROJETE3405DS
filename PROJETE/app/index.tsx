7e9cd254dc148125efb1cbc3acd5f503fda45915
Pedroam24 <pedroalmartins09@gmail.com>
2025-07-04 09:37:58 -0300

    adicionado titulo e botão

--- PROJETE/app/index.tsx
+++ PROJETE/app/index.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 
-export default function Index() {
+export default function Index({navigation}) {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        paddingTop: 50,
+        backgroundColor: "#335C67",
       }}
     >
-      <Text>Are you sure?</Text>
+      
+      <Text style=
+      {{
+          fontSize: 32,
+          fontWeight: "bold",
+          color: "#E09F3E",
+      }}>
+          Read-Cycle</Text>
+
+ <Button title="Ir para a outra página"/>
     </View>
   );
 }
