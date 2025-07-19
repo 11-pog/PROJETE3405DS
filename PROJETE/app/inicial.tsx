7c6453bdd1fcd73500d1583f9c062e429b49206f
Pedroam24 <pedroalmartins09@gmail.com>
2025-07-18 22:14:04 -0300

    Melhorado a tela inicial

--- PROJETE/app/inicial.tsx
+++ PROJETE/app/inicial.tsx
@@ -5,15 +5,19 @@ import { Text, View, Button } from "react-native";
 import Login from './pages/login/login';
 import Cadastrar from './pages/cadastrar/cadastrar'
 export default function Inicial() {
+
+
   return (
+
     <View
       style={{
         flex: 1,
         justifyContent: "flex-start",
         alignItems: "center",
         paddingTop: 50,
-        backgroundColor: "#335C67",
+        backgroundColor: "#F5F5F5",
       }}
+      
     >
       
       <Text style=
@@ -24,14 +28,51 @@ export default function Inicial() {
       }}>
           Read-Cycle</Text>
 
-    <button  onClick={BotaoLogin}>
-      Login
+    <button
+    style={{
+    backgroundColor: "#335C67",
+  color:"#F5F5F5",
+  border: 0,
+  borderRadius: 9999, 
+  padding: 10, 
+  fontSize: 16,
+  marginTop: 105,
+
+
+    }}  
+
+    onClick={BotaoLogin}>
+      Entrar
     </button>
     
-    <button onClick={BotaoCadastrar} >
+    <button 
+     style={{
+    backgroundColor: "#335C67",
+    color:"#F5F5F5",
+    border: 0,
+    borderRadius: 9999, 
+    padding: 10, 
+    fontSize: 16,
+    marginTop: 20,
+    
+     }}
+    onClick={BotaoCadastrar} >
       Cadastrar
     </button>
-    
+
+    <text
+    style={{
+          fontSize: 16,
+          fontWeight: "bold",
+          color: "#9E2A2B",
+          marginTop: 20,
+
+
+    }}>
+  Troque livros. 
+Troque ideias. 
+Toque o mundo.
+    </text>
     </View>
   );
 
@@ -41,6 +82,16 @@ export default function Inicial() {
   function BotaoCadastrar(){
     navigate ('/pages/cadastrar/cadastrar');
   }
+  
+  //encontrar uma forma de usar essa function para não ficar repetindo em todos os botões
+   function Btn(){
+  backgroundColor: "#335C67";
+  color:"#F5F5F5";
+  border: 0;
+  borderRadius: 9999; 
+  padding: 10; 30;
+  fontSize: 16;
+      }
 }
 
 
