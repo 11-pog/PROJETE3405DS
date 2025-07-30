3642a9ba15b38a3469d918c0069272df7b935e56
Slept0122 <luis.henrique.01@hotmail.com>
2025-07-30 12:01:30 -0300

    Atualizado

--- DjangoTeste/Aplicativo/views.py
+++ DjangoTeste/Aplicativo/views.py
@@ -43,7 +43,7 @@ class LoginUsuario(APIView):
             
 class Buscadelivro(APIView):
     def get(self, request):
-        isbn = request.query_params.get('isbn')
+        isbn = 9780545069670 #colocar isbn manual(codigo anterior), request.query_params.get('isbn')
         if not isbn:
             return Response({"erro": "ISBN não fornecido"}, status=400)
     
@@ -54,17 +54,29 @@ class Buscadelivro(APIView):
             return Response({"erro": "Erro ao consultar a API externa"}, status=500)
         
         dados = resposta.json()
-        if not dados["docs"]:
+        if not dados:
             return Response({"erro": "Livro não encontrado"}, status=404)
 
         livro = dados["docs"][0]
 
+        authors = livro.get("authors", [])
+        if authors:
+           autorkey = authors[0].get("key", None)
+
+           if autorkey:
+             url_autor = f"https://openlibrary.org{autorkey}.json"
+             resposta_autor = requests.get(url_autor)
+
+             if resposta_autor.status_code == 200:
+                 autor = resposta_autor.json().get("name", "Desconhecido")
+
+
         resultado = {
             "titulo": livro.get("title", "Título não encontrado"),
-            "autor(a)": livro.get("author_name", ["Autor desconhecido"])[0], #nao tem nesse url json
+            "autor(a)": autor, #outro URL
             "editor(a)": livro.get("publishers", ["Editora desconhecida"])[0],
             "ano_publicacao": livro.get("publish_date", "Ano desconhecido"),
-            "Descricao": livro.get("description", "Descrição não disponível"), #nao tem nesse url json
+            "Descricao": livro.get("value", "Descrição não disponível"), 
         }
-
+            
         return Response(resultado, status=200)
