4f6f4f1d061650bc356bb857b11284ba59b5d899
11-pog <alvessamuel872@gmail.com>
2025-09-18 16:50:47 -0300

    esqueci de revertes as config q eu tinha feito

--- DjangoTeste/SiteTeste/settings.py
+++ DjangoTeste/SiteTeste/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'decoy.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
