4c724bb585a6b5ffe541ae8cb20c0108d3531c7b
11-pog <alvessamuel872@gmail.com>
2025-09-21 17:11:53 -0300

    Levemente melhorado validate no serializer de login

--- DjangoTeste/Aplicativo/serializers/token_serializer.py
+++ DjangoTeste/Aplicativo/serializers/token_serializer.py
@@ -1,29 +1,28 @@
 # Aplicativo/serializers.py
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework.exceptions import AuthenticationFailed
 
 User = get_user_model()
 
 class LoginEmailTokenSerializer(TokenObtainPairSerializer):
     username_field = User.EMAIL_FIELD
-
+    
     def validate(self, attrs):
         email = attrs.get("email")
         password = attrs.get("password")
-
-        user = User.objects.filter(email=email).first()
         
+        user = authenticate(username=email, password=password)
         if user is None or not user.check_password(password):
-            raise serializers.ValidationError("Email ou senha inválidos")
+            raise AuthenticationFailed("Email ou senha inválidos")
         
         attrs[self.username_field] = user.email
         data = super().validate(attrs)
-
+        
         data["user"] = {
             "id": user.id,
             "email": user.email,
             "username": user.username
         }
-
+        
         return data
