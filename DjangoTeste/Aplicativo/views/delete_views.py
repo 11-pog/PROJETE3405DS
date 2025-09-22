e23a95ec3a8446d70a236787092206939f0c9254
11-pog <alvessamuel872@gmail.com>
2025-09-22 03:39:29 -0300

    Consertado import em delete_views

--- DjangoTeste/Aplicativo/views/delete_views.py
+++ DjangoTeste/Aplicativo/views/delete_views.py
@@ -1,7 +1,7 @@
 # views.py
 from rest_framework import generics, permissions
 from Aplicativo.models.publication_models import Publication
-from Aplicativo.serializers.publication_serializers import PublicationSerializer
+from Aplicativo.serializers.publication_serializer import PublicationSerializer
 
 class PublicationDeleteView(generics.DestroyAPIView):
     queryset = Publication.objects.all()
