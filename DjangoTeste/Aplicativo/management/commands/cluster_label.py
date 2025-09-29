e52d4d816dd7a1f3d29c5bc0bb758e092a39366e
11-pog <alvessamuel872@gmail.com>
2025-09-29 16:19:10 -0300

    top 10 commits

--- DjangoTeste/Aplicativo/management/commands/cluster_label.py
+++ DjangoTeste/Aplicativo/management/commands/cluster_label.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from Aplicativo.models.publication_models import Publication
-from DjangoTeste.Aplicativo.ml.clustering import cluster_publications, cluster_users
+from Aplicativo.ml.clustering import cluster_publications, cluster_users
 from sklearn.cluster import KMeans
 import numpy as np
 
